de877ad60055e963d96584be1cf4823406826751
Mr.Nikhil <115720074+nikhil7761@users.noreply.github.com>
2023-01-26 19:12:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace cross
+{
+
+public delegate void calcualtion(int a,int b);
+class program
+{
+public static void addition(int a,int b){
+int result=a+b;
+Console.WriteLine("addition result is {0}",result);
+}
+public static void substraction(int a,int b){
+int result=a-b;
+Console.WriteLine("addition result is {0}",result);
+}
+
+
+public static void Main(String[] args)
+{
+calcualtion obj=new calcualtion(program.addition);
+obj.Invoke(23,12);
+obj=substraction;
+obj(10,3);
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
